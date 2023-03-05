02767838d8a364a08d51a509bbcb296b0c8e82b1
wolfy1339 <webmaster@wolfy1339.com>
2023-03-05 10:51:22 -0500

    build: add scripts to generate types

---
+++ scripts/generate-types.js
@@ -0,0 +1,95 @@
+import { readdir, mkdir, rm, writeFile, copyFile } from "node:fs/promises";
+import { basename } from "node:path";
+
+import prettier from "prettier";
+import openapiTS from "openapi-typescript";
+
+if (!process.env.OCTOKIT_OPENAPI_VERSION) {
+  throw new Error("OCTOKIT_OPENAPI_VERSION is not set");
+}
+
+run();
+
+const packageDefaults = {
+  publishConfig: {
+    access: "public",
+  },
+  version: "0.0.0-development",
+  main: "",
+  types: "types.d.ts",
+  author: "Gregor Martynus (https://twitter.com/gr2m)",
+  license: "MIT",
+  octokit: {
+    "openapi-version": process.env.OCTOKIT_OPENAPI_VERSION.replace(/^v/, ""),
+  },
+};
+
+async function run() {
+  await rm("packages", { recursive: true }).catch(() => {});
+  await mkdir("packages");
+
+  const files = await readdir("generated");
+  for (const fileName of files) {
+    if (!/\.json$/.test(fileName)) continue;
+
+    const name = basename(fileName, ".json");
+
+    const packageName =
+      name === "api.github.com" ? "openapi-webhooks-types" : `openapi-webhooks-types-${name}`;
+
+    await mkdir(`packages/${packageName}`);
+    await writeFile(
+      `packages/${packageName}/package.json`,
+      prettier.format(
+        JSON.stringify({
+          name: `@octokit/${packageName}`,
+          description: `Generated TypeScript definitions based on GitHub's OpenAPI spec for ${name}`,
+          repository: {
+            type: "git",
+            url: "https://github.com/octokit/openapi-types.ts.git",
+            directory: `packages/${packageName}`,
+          },
+          ...packageDefaults,
+        }),
+        { parser: "json-stringify" }
+      )
+    );
+    await writeFile(
+      `packages/${packageName}/README.md`,
+      prettier.format(
+        `
+# @octokit/${packageName}
+
+> Generated TypeScript definitions based on GitHub's OpenAPI spec ${
+          packageName === "openapi-webhooks-types" ? "" : `for ${name}`
+        }
+
+This package is continously updated based on [GitHub's OpenAPI specification](https://github.com/github/rest-api-description/) 
+
+## Usage
+
+\`\`\`ts
+import { components } from "@octokit/${packageName}";
+
+type Repository = components["schemas"]["full-repository"]
+\`\`\`
+
+## License
+
+[MIT](LICENSE)
+`,
+        { parser: "markdown" }
+      )
+    );
+
+    await copyFile("LICENSE", `packages/${packageName}/LICENSE`);
+
+    await writeFile(
+      `packages/${packageName}/types.d.ts`,
+      prettier.format(await openapiTS(`generated/${name}.json`), {
+        parser: "typescript",
+      })
+    );
+    console.log(`packages/${packageName}/types.d.ts written`);
+  }
+}
---
+++ scripts/update-packages.js
@@ -0,0 +1,36 @@
+import { writeFile, readdir, readFile } from "node:fs/promises";
+import prettier from "prettier";
+
+if (!process.env.OCTOKIT_OPENAPI_VERSION) {
+  throw new Error("OCTOKIT_OPENAPI_VERSION is not set");
+}
+
+const pkg = JSON.parse((await readFile("package.json")).toString());
+
+updatePackage();
+
+async function updatePackage() {
+  // set semantic-release configuration of npm packages
+  const items = await readdir("packages");
+  const packages = items.filter((item) => item.startsWith("openapi-webhooks-types"));
+
+  pkg.release.plugins = [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ].concat(
+    packages.map((packageName) => {
+      return [
+        "@semantic-release/npm",
+        {
+          pkgRoot: `packages/${packageName}`,
+        },
+      ];
+    })
+  );
+
+  await writeFile(
+    "package.json",
+    prettier.format(JSON.stringify(pkg), { parser: "json-stringify" })
+  );
+}
