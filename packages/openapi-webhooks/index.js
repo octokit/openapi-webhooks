a09854e3511982837fdc893ade23dfdc009c03bc
wolfy1339 <webmaster@wolfy1339.com>
2024-02-16 10:23:47 -0500

    build: generate files

--- packages/openapi-webhooks/index.js
+++ packages/openapi-webhooks/index.js
@@ -1,3 +1,12 @@
 module.exports = {
-  schemas: {},
+  schemas: {
+    ["api.github.com"]: require("./generated/api.github.com.json"),
+    ["ghec"]: require("./generated/ghec.json"),
+    ["ghes-3.10"]: require("./generated/ghes-3.10.json"),
+    ["ghes-3.11"]: require("./generated/ghes-3.11.json"),
+    ["ghes-3.12"]: require("./generated/ghes-3.12.json"),
+    ["ghes-3.8"]: require("./generated/ghes-3.8.json"),
+    ["ghes-3.9"]: require("./generated/ghes-3.9.json"),
+    ["github.ae"]: require("./generated/github.ae.json"),
+  },
 };
